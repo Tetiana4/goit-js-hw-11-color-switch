c217a7688a37d7d4228d8296aacce6ef0443bfb7
Tetiana Shevchuk <shevchuktetiana4@gmail.com>
2021-07-08 21:52:46 +0200

    Добавляет рабочие файлы

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 import './sass/main.scss';
+
+const buttons = {
+    start: document.querySelector('button[data-start]'),
+    stop: document.querySelector('button[data-stop]')
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+let interval = undefined;
+
+buttons.start.addEventListener('click', evt => interval = interval ?
+    interval : 
+    setInterval(() => getRandomHexColor(), 1000))
+
+buttons.stop.addEventListener('click', evt => clearInterval(interval))
+
